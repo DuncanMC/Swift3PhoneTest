1f83a95267fbfb5069aa261b4950d203b2ab2835
Duncan Champney <DChampney@wareto.com>
2016-11-09 11:08:08 -0500

    Initial Commit

---
+++ Swift3PhoneTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift3PhoneTest
+//
+//  Created by Duncan Champney on 11/9/16.
+//  Copyright © 2016 Duncan Champney. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
