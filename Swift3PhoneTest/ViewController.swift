6a085bca43fecb612a7a0a1e1b41310a00eb182a
Duncan Champney <DChampney@wareto.com>
2016-11-22 10:50:45 -0500

    Added lots of code documentation.

--- Swift3PhoneTest/ViewController.swift
+++ Swift3PhoneTest/ViewController.swift
@@ -10,27 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
   
+  //MARK: - IBOutlets
+
   @IBOutlet weak var string1Field: UITextField!
   @IBOutlet weak var optionalStringField: UITextField!
-  
   @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+  @IBOutlet weak var timeValueLabel: UILabel!
+
+  
+  //MARK: - Properties
+  
+  ///By making your timer a weak optional, as soon as the timer ends or is invalidated,
+  ///it gets released and set to nil.
   weak var timer: Timer?
   
   var dataObject: DataObject
   var startTime: Double = 0
   var time: Double = 0
   
+  /**
+   When a view controller is involked from a storyboard it gets created with `init?(coder:)`. We override this method so we can load our DataObject from user UserDefaults. (For this demo project we save our encoded DataObject to UserDefaults. For a more complex data object you'd probably want to save your encoded data to a file, since UserDefaults is not inteded to hold large amounts of data.)
+ */
   
+  //MARK: - init/deinit
+  
+  ///The deinit function gets called when the object is about to be deallocated. We use it to stop our running timer. Note that this approach only works if the timer closure does not hold a strong reference to self.
+  
+  deinit {
+    timer?.invalidate()
+  }
+
   required init?(coder aDecoder: NSCoder) {
     let defaults = UserDefaults.standard
+    
+    //Try to load NSData for our DataObjetc from UserDefaults
     if let data = defaults.object(forKey:"dataObjectData") as? Data {
-      dataObject = NSKeyedUnarchiver.unarchiveObject(with: data) as! DataObject
+      
+      //Convert the NSData to a DataObject. If it fails, the init should fail.
+      guard let temp = NSKeyedUnarchiver.unarchiveObject(with: data) as? DataObject else {
+        return nil
+      }
+      dataObject = temp
     } else {
       dataObject = DataObject()
+    
     }
     super.init(coder: aDecoder)
   }
   
+  //MARK: - Instance methods
+ 
   func saveDataObject() {
     dataObject.string1 = string1Field.text ?? ""
     var string: String! = optionalStringField.text
@@ -39,51 +68,83 @@ class ViewController: UIViewController {
     }
     dataObject.optionalString = string
 
+    //This code converts our DataObject to NSData so we can save it. In order to do this the object must conform to NSCoding or NSSecureCoding.
     let data = NSKeyedArchiver.archivedData(withRootObject: dataObject)
     UserDefaults.standard.set(data, forKey: "dataObjectData")
   }
   
-  @IBOutlet weak var timeValueLabel: UILabel!
-  
+  //MARK: - overriden UIViewController methods
+
   override func viewWillAppear(_ animated: Bool) {
+    
+    //When our view first appears, display the values from our DataObject into our text fields.
     string1Field.text = dataObject.string1
     optionalStringField.text = dataObject.optionalString ?? ""
   }
   
+  /**
+   This viewDidAppear() function illustrates using the new-to-iOS10 variant of NSTimer (Timer) that takes a block (closure) rather than a selector.
+   
+   If your timer closure references self then you should use a capture group (e.g. `[weak self] in`) so you don't create a retain cycle where the closure retains the object and the timer retains the closure.
+ */
+  
   override func viewDidAppear(_ animated: Bool) {
     startTime = Date().timeIntervalSinceReferenceDate
-//    timer = Timer.scheduledTimer(withTimeInterval: 0.5,
-//                                 repeats: true) {
-//                                  [weak self] timer in
-//                                  guard let weakSelf = self else {
-//                                    return
-//                                  }
-//                                  //Total time since timer started, in seconds
-//                                  weakSelf.time = Date().timeIntervalSinceReferenceDate - weakSelf.startTime
-//                                  
-//                                  //The rest of your code goes here
-//                                  let timeString = String(format: "%.2f", weakSelf.time)
-//                                  weakSelf.timeValueLabel.text = timeString
-//    }
+    timer = Timer.scheduledTimer(withTimeInterval: 0.05,
+                                 repeats: true) {
+                                  [weak self] timer in
+                                  
+                                  //This is defensive coding that unwraps our weak copy of self. The guard statement exits if self has been released. (That would happen if the timer is still runing but the object it points to has been released.)
+                                  guard let strongSelf = self else {
+                                    return
+                                  }
+                                  //Total time since timer started, in seconds
+                                  strongSelf.time = Date().timeIntervalSinceReferenceDate - strongSelf.startTime
+                                  
+                                  let timeString = String(format: "%.2f", strongSelf.time)
+                                  strongSelf.timeValueLabel.text = timeString
+    }
   }
   
+  //MARK: - IBAction methods 
+  
+  /**
+   This button action shows how to manage an activity indicator when you have a long-running block of code that runs **on the main thread**.
+   
+   The pattern is as follows:
+   1. Start the activity indicator
+   2. Wrap your time-consuming code in an async call so you serve the event loop before it begins executing. (this lets the activity indicator start spinning.)
+      - Once your time-consuming task is complete, call activityIndicator.stopAnimating() inside your async block
+   */
+  
   @IBAction func handleButton(_ sender: UIButton) {
+    
     activityIndicator.startAnimating()
     sender.isEnabled = false
-    DispatchQueue.main.asyncAfter(deadline: .now() + 0.75, execute: {
-      DispatchQueue.main.async{
-        [weak self] in
-        guard let strongSelf = self else {
-          return
-        }
-        strongSelf.setEditing(false, animated: true)
-        strongSelf.saveDataObject()
-        strongSelf.activityIndicator.stopAnimating()
-        sender.isEnabled = true
+    DispatchQueue.main.async() {
+      [weak self] in
+      guard let strongSelf = self else {
+        return
       }
-    })
+      //This call to usleep simulates a time-consuming task on the main thread. It sleeps for 1/2 second.
+      //Note that you should never use sleep/usleep on the main thread in real production code.
+      usleep(500_000)
+      strongSelf.setEditing(false, animated: true)
+      strongSelf.saveDataObject()
+      
+      //Once the time-consuming task is complete, stop the activity indicator from spinning (from inside the call to DispatchQueue.main.async() .)
+      strongSelf.activityIndicator.stopAnimating()
+      sender.isEnabled = true
+    }
   }
 }
+
+//MARK: - ViewController UITextFieldDelegate protocol methods
+
+/**
+ Using an extension is a good way to group your object's methods. If your object conforms to a protocol that has methods, place those methods in an extension. You can also use extensions to group sets of related methods
+ */
+
 extension ViewController: UITextFieldDelegate {
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
     self.setEditing(false, animated: true)
